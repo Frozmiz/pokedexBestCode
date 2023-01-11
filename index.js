274b99063eb5d47c646f6c993a2754b71562fbb1
Alejandro <frozmiz@live.com>
2023-01-11 01:36:16 +0100

    feat

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
-    
 // Inicializamos las variables principales.
 
-let characters = [];
+let pokemones = [];
 let cuantosPokemones = 23;
 
-// Nos aseguramos de que todos los pokemon (characters) se han cargado en orden en el array.
+// Nos aseguramos de que todos los pokemon (pokemones) se han cargado en orden en el array.
 
 const getPokemons = async () => {
-    
-    for (let i = 1; i <= cuantosPokemones; i++) {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${+i}/`);
-        const result = await res.json();
-        characters.push(result);         
-    } 
+  for (let i = 1; i <= cuantosPokemones; i++) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${+i}/`);
+    const result = await res.json();
+    pokemones.push(result);
+  }
 
-    crearPokemon();
-    console.log(characters);
+  
+  crearPokemon();
+  crearBotones();
+  console.log(pokemones);
 };
 
+const crearBotones = () => {
 
-const crearPokemon = async() => {
+  const buttons$$ = document.querySelector(".buttons");
+  const types = [];
 
-
-    for (const character of characters) {
-
-        
-        const card$$ = document.createElement("div");
-        const name$$ = document.createElement("h3");
-        const imagen$$ = document.createElement("img");
-        const type$$ = document.createElement("p");
-        const number$$ = document.createElement("p");
-        const imagenContainer$$ = document.createElement("div");
-    
-
-        imagenContainer$$.classList.add("img-container");
-        card$$.classList.add("card");
-        name$$.textContent = character.name;
-        imagen$$.src = character.sprites.front_default;
-        number$$.textContent = `# ${character.id}`;
-        type$$.textContent = character.types[0].type.name;
-        
-
-        imagenContainer$$.appendChild(imagen$$);
-        card$$.appendChild(name$$);
-        card$$.appendChild(imagenContainer$$);
-        card$$.appendChild(number$$);
-        card$$.appendChild(type$$);
-
-        pokemonContainer.appendChild(card$$);
-
-
+  for (const pokemon of pokemones) {
+    for (const type of pokemon.types) {
+      if (!types.includes(type.type.name)) {
+        types.push(type.type.name);
+      }
     }
+  }
+  console.log(types);
 
-}
+  for (tipos of types) {
+
+        const button$$ = document.createElement("button");
+
+        button$$.textContent = tipos;
+
+        buttons$$.appendChild(button$$);
+
+  };
+
+};
+
+const crearPokemon = async () => {
+  for (const pokemon of pokemones) {
+    const card$$ = document.createElement("div");
+    const name$$ = document.createElement("h3");
+    const imagen$$ = document.createElement("img");
+    const type$$ = document.createElement("p");
+    const number$$ = document.createElement("p");
+    const imagenContainer$$ = document.createElement("div");
+
+    imagenContainer$$.classList.add("img-container");
+    card$$.classList.add("card");
+    name$$.textContent = pokemon.name;
+    imagen$$.src = pokemon.sprites.front_default;
+    number$$.textContent = `# ${pokemon.id}`;
+    type$$.textContent = pokemon.types[0].type.name;
+
+    imagenContainer$$.appendChild(imagen$$);
+    card$$.appendChild(name$$);
+    card$$.appendChild(imagenContainer$$);
+    card$$.appendChild(number$$);
+    card$$.appendChild(type$$);
+
+    pokemonContainer.appendChild(card$$);
+
+    // console.log(pokemon.types);
+  }
+};
 
 const pokemonContainer = document.querySelector(".container");
 
